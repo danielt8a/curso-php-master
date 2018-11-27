3e43150a48d008d184d2d488153b54e5aff3a92a
DamasoDaniel <damasodt8a@gmail.com>
2018-11-26 22:31:31 -0600

    Comentarios en PHP

--- holaMundo.php
+++ holaMundo.php
@@ -1,7 +1,26 @@
-<h1>Hola mundo, esto es HTML</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Imprimir por pantalla - Master PHPh1</title>
+</head>
+<body>
+    <h1>Master en PHP</h1>
+    <?php
+        echo '<h3>Listado de videojuegos</h3>';
+        echo '<ul>'
+                .'<li>GTA</li>'
+                .'<li>Fifa</li>'
+                .'<li>Mario Bros</li>'
+                .'<li>Mario Bros'
+                ;
+    ?>
 
-<?php
+    <!-- Forma corta de hacer un echo -->
+    <?= 'Aprendiendo el excelente lenguaje PHP' ?>
 
-echo '<h2>Hola mundo</h2>';
+</body>
+</html>
 
-?>
