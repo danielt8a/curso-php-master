6617b3d9a15c9b28b13f871d7ee45f6c17a9bb18
DamasoDaniel <damasodt8a@gmail.com>
2018-12-03 19:48:24 -0600

    Condicional if

---
+++ condicionales.php
@@ -0,0 +1,22 @@
+<?php
+// CONDICIONALES
+/* 
+    if(condicion){
+        instrucciones
+
+    }else {
+        otras instrucciones
+    }
+*/
+
+$color = "rojo";
+
+if ($color == "rojo") {
+    echo "El color es rojo";
+} else {
+    echo "El color no es rojo";
+}
+
+
+?>
+
