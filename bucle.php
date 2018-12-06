5c1c011180b2722be96868d89de1a4b963df828a
DamasoDaniel <damasodt8a@gmail.com>
2018-12-05 23:23:23 -0600

    Segundo ejemplo con while

--- bucle.php
+++ bucle.php
@@ -13,11 +13,32 @@ while(condicion){
 $numero = 0;
 while ($numero <= 100) {
     echo "$numero";
-    if ($numero != 100){
+    if ($numero != 100) {
         echo ", ";
     }
     $numero++;
 }
 
+echo "<br>";
+
+//Segundo ejemplo con while
+
+if (isset($_GET['numero'])) {
+    //Cambiar el tipo string a int
+    $numero = (int)$_GET['numero'];
+} else {
+    $numero = 1;
+}
+
+echo "<h1>Tabla de multiplicar del número $numero</h1>";
+
+$contador = 0;
+while ($contador <= 10) {
+    echo "$numero x $contador = " . ($numero * $contador) . "<br>";
+    $contador++;
+}
+
+
 ?>
+
 
