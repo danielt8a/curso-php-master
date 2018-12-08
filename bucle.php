40f29d367df0ffefc6629bfa14205ac779a36b6a
DamasoDaniel <damasodt8a@gmail.com>
2018-12-07 23:20:43 -0600

    Bucle For

--- bucle.php
+++ bucle.php
@@ -62,6 +62,15 @@ for (variable contador, condición, incremento contador){
 
 */
 
+$resultado = 0;
+for($i = 0; $i <= 100; $i++){
+    $resultado += $i;
+    echo "<p>$resultado</p>";
+}
+
+echo "<h1>El resultado es: $resultado</h1>";
+
+
 
 ?>
 
